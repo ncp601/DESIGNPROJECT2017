88ec314ae7dfba15e68945e714c1de78adec516b
ncp601 <ncp601@mun.ca>
2017-03-29 15:08:09 -0230

    Update StairsComponentFactory.java

--- src/Factories/StairsComponentFactory.java
+++ src/Factories/StairsComponentFactory.java
@@ -1,23 +1,60 @@
+
 package Factories;
 
 import FloorComponent.*;
 
 public class StairsComponentFactory extends AbstractFloorComponentFactory{
-	
-	@Override
-	public FloorComponent getComponent(String componentType){
-		if(componentType == null){
-			return null;
+		
+		@Override
+		public FloorComponent getComponent(String componentType){
+			if(componentType == null){
+				return null;
+			}
+			
+			else if(componentType == "STAIRSSU"){
+				return new StairsStraightFacingUp(true);
+			}
+			
+			else if(componentType == "STAIRSSD"){
+				return new StairsStraightFacingDown(true);
+			}
+			
+			else if(componentType == "STAIRSSR"){
+				return new StairsStraightFacingRight(true);
+			}
+			
+			else if(componentType == "STAIRSSL"){
+				return new StairsStraightFacingLeft(true);
+			}
+			
+			else
+				return null;
 		}
 		
-		else
-			return null;
-	}
-
-	@Override
-	public FloorComponent getGridComponent(String componentType) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+		@Override
+		public FloorComponent getGridComponent(String componentType){
+			if(componentType == null){
+				return null;
+			}
+			
+			else if(componentType == "STAIRSSU"){
+				return new StairsStraightFacingUp(false);
+			}
+			
+			else if(componentType == "STAIRSSD"){
+				return new StairsStraightFacingDown(false);
+			}
+			
+			else if(componentType == "STAIRSSR"){
+				return new StairsStraightFacingRight(false);
+			}
+			
+			else if(componentType == "STAIRSSL"){
+				return new StairsStraightFacingLeft(false);
+			}
+			
+			else
+				return null;
+		}
+		
 }
